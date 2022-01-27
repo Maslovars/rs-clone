78b4aedf434c99aa7613a26f84a7fe43818e49dc
Maslovars <maslovars@gmail.com>
2022-01-27 11:36:17 +0300

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        RS-CLONE
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">RS-CLONE</header>
+        </div>
+    );
 }
 
 export default App;
