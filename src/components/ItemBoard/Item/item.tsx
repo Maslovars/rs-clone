e5911830c8b7e741ebcb7be1c3d210fa2dea332e
zhirkovpetr <zhirkovpetr95@gmail.com>
2022-02-10 13:07:01 +0300

    feat: add itemBoard and item

---
+++ src/components/ItemBoard/Item/item.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ItemType } from '../../../constants/items';
+import '../itemBoard.scss';
+
+type ItemPropsType = {
+    item: ItemType | null;
+    onClick: (item: ItemType | null, i: number) => void;
+    i: number;
+    clickedIndex: number | null;
+    highlight: boolean;
+};
+
+function Item(props: ItemPropsType) {
+    const { item, onClick, i, clickedIndex, highlight } = props;
+    if (!item) {
+        return (
+            <div
+                aria-label="button"
+                tabIndex={0}
+                role="button"
+                onClick={() => onClick && onClick(null, i)}
+                className="item"
+            />
+        );
+    }
+
+    return (
+        <div
+            aria-label="button"
+            tabIndex={0}
+            role="button"
+            onClick={() => onClick && onClick(item, i)}
+            className={`item ${clickedIndex === i ? 'clicked' : ''} ${
+                highlight ? 'highlight' : ''
+            }`}
+        >
+            <div className="item-stats">
+                <div className="item-damage">{item.damage}</div>
+                <div className="item-coin">{item.coins}</div>
+            </div>
+            {/*  <img src={props.item.icon} /> */}
+        </div>
+    );
+}
+
+export default Item;
