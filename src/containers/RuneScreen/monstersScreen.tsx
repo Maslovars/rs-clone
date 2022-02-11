d2a54e90d81e2238b9dcfa7dc179c9b51128cf48
zhirkovpetr <zhirkovpetr95@gmail.com>
2022-02-11 18:18:31 +0300

    feat: add rune

---
+++ src/containers/RuneScreen/monstersScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { AppStateType } from '../../app/redux-store';
+import { RunesListType } from '../../constants/runes';
+import Rune from '../../components/Rune/rune';
+
+type RuneScreenPropsType = {
+    onRuneBuy: (rune: RunesListType) => void;
+};
+
+const runeScreen = (props: RuneScreenPropsType) => {
+    const runes = useSelector<AppStateType, RunesListType[]>(
+        (state) => state.rune.runes,
+    );
+    const coin = useSelector<AppStateType, number>((state) => state.coin.coins);
+
+    const { onRuneBuy } = props;
+    return runes.map((rune) => {
+        const canBuy = coin >= rune.price;
+
+        return (
+            <Rune
+                key={rune.name}
+                rune={rune}
+                canBuy={canBuy}
+                onClick={() => {
+                    if (canBuy) {
+                        onRuneBuy(rune);
+                    }
+                }}
+            />
+        );
+    });
+};
+
+export default runeScreen;
