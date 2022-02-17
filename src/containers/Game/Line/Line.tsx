32aabb55afdebbae6d380e88c8d16cfd8eea8768
zhirkovpetr <zhirkovpetr95@gmail.com>
2022-02-17 13:11:26 +0300

    feat: continued to add game component

---
+++ src/containers/Game/Line/Line.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react';
+import { Mesh } from 'three';
+import { useFrame } from '@react-three/fiber';
+
+export function Line() {
+    const mesh = useRef<Mesh>();
+    useFrame(() => {
+        if (mesh.current) {
+            mesh.current.rotation.y += 0.8
+            mesh.current.rotation.x += 0.8
+
+            if (mesh.current.position.x < 10) {
+                mesh.current.position.x += 0.1
+            } else {
+                mesh.current.position.x = -10
+            }
+        }
+    })
+
+    return (
+        <mesh ref={mesh}>
+            <boxGeometry args={[1, 0.1, 1]} />
+            <meshStandardMaterial color="#dd7777" />
+        </mesh>
+    )
+}
